d2e5f3110b5388e05f360860bf5015df5bd4a0ef
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-11-28 20:14:43 -0400

    Add component Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,41 @@
+import { Heading, HStack, Image } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
+import Logo from './../logo.svg';
+interface IHeaderProps {
+  menu?: boolean;
+}
+export const Header = ({ menu }: IHeaderProps) => {
+  return (
+    <HStack
+      justifyContent="space-between"
+      bg="blackAlpha.800"
+      w="full"
+      height="16"
+      px={4}
+    >
+      <HStack gap={3}>
+        <Image src={Logo} alt="logo" />
+        <Heading
+          color="white"
+          size="lg"
+          fontFamily="Montserrat"
+          fontWeight={900}
+        >
+          <Link to={menu ? '/products' : '/'}>
+            MacMagazine
+          </Link>
+        </Heading>
+      </HStack>
+      <HStack
+        display={menu ? 'flex' : 'none'}
+        color="whiteAlpha.800"
+        fontFamily="Montserrat"
+        gap={5}
+      >
+        <Link to="/products">Produtos</Link>
+        <Link to="/map">Mapa</Link>
+      </HStack>
+    </HStack>
+  );
+};
