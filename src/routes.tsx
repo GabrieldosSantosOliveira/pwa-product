b5f9a7c9a920c1efa3f5c227f2870b9788e6a9f9
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-05 22:52:11 -0400

    Update routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,16 +5,24 @@ import {
 
 import { AuthProvider } from './context/AuthContext';
 import { PrivateRoute } from './PrivateRoute';
+import { AddProducts } from './screens/AddProduct';
 import { Home } from './screens/Home';
 import { Map } from './screens/Map';
+import { Offline } from './screens/Offline';
 import { Products } from './screens/Products';
 export const Routes = () => {
   return (
     <RoutesReactRouter>
       <Route path="/" element={<Home />} />
+      <Route path="/offline" element={<Offline />} />
       <Route element={<AuthProvider />}>
         <Route element={<PrivateRoute redirectTo="/" />}>
           <Route path="/products" element={<Products />} />
+          <Route
+            path="/create-products"
+            element={<AddProducts />}
+          />
+
           <Route path="/map" element={<Map />} />
         </Route>
       </Route>
