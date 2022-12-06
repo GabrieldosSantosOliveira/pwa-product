d00242468494d8ecc7aba81240f9433e30bc2e6c
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-05 22:55:01 -0400

    Add apiAuth

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,12 +1,34 @@
 import axios from 'axios';
 
+import { refreshToken } from './refreshToken';
+
 const apiContext = (token?: string) => {
-  return axios.create({
+  const api = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
     headers: {
       Authorization: `Bearer ${token}`
     }
   });
+  api.interceptors.response.use(
+    response => response,
+    async error => {
+      if (error.response.status === 401) {
+        try {
+          console.log('401');
+          const { token } = await refreshToken();
+          error.config.headers['Authorization'] =
+            'Bearer ' + token;
+          console.log('401', error.config);
+          return axios.request(error.config);
+        } catch (err) {
+          console.log(err);
+          return Promise.reject(err);
+        }
+      }
+      return Promise.reject(error);
+    }
+  );
+  return api;
 };
 
 export { apiContext };
